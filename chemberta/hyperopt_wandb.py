b068da570f3afe686522668c8532c2d48cefb6e4
Aron <aronpjansen@gmail.com>
2022-03-18 15:10:34 +0100

    Add save_model=True, remove TODO about learning rate logging

--- chemberta/hyperopt_wandb.py
+++ chemberta/hyperopt_wandb.py
@@ -8,16 +8,19 @@ logging.set_verbosity_error()
 
 MODEL_CHECKPOINT = 'DeepChem/ChemBERTa-77M-MTR'
 WANDB_USERNAME = 'apjansen'
-NUM_RUNS = 4
+NUM_RUNS = 25
 FILENAME = '../data/all_carboxylics.csv'
   
 def train(config=None):
     """Wrapper to pass to wandb agent."""
-    with wandb.init( config=config):
+    with wandb.init(config=config):
         config = wandb.config
         
         wandb_callback = wandb.keras.WandbCallback(
-            monitor='val_root_mean_squared_error', save_weights_only=True)
+            monitor='val_root_mean_squared_error',
+            save_model=True,
+            save_weights_only=True,
+        )
 
         fine_tune(**config, wandb_callback=wandb_callback)
 
@@ -31,15 +34,13 @@ sweep_config['parameters'] = {
     'checkpoint': {'value': MODEL_CHECKPOINT},
     'filename': {'value': FILENAME},
     'task_id': {'value': 0},
-    'epochs': {'value': 2},
+    'epochs': {'value': 20},
     'learning_rate': {'values': [3e-6, 1e-5, 2e-5, 5e-5, 1e-4]},
     'decay_rate': {'values': [1., .95, .9]},  # each epoch decay lr by this factor
     'batch_size': {'values': [64, 128, 256]}, # batch size 512 can't be handled by colab
     'weight_decay': {'value': 0.},
     'patience': {'value': 5},
 }
-# TODO:
-# - add logging of learning rate to be sure it's working as intended
 
 def main():
     sweep_id = wandb.sweep(
