b38169fa2c0667310f9df6f5655cb61bb81a8d2d
Aron <aronpjansen@gmail.com>
2022-05-16 10:28:06 +0200

    Use best frozen base model for predictions

--- chemberta/predict_angle.py
+++ chemberta/predict_angle.py
@@ -10,41 +10,9 @@ from model_loader import load_model_and_tokenizer
 from model_evaluation import get_best_sweep, restore_model_wandb
 
 MODEL_CHECKPOINT = 'DeepChem/ChemBERTa-77M-MTR'
-SWEEP_ID  = 'apjansen/chemberta/5ble16ck/'
+SWEEP_ID  = 'apjansen/chemberta/5pw5mcyz/'
 DATA_FILE = '../data/carbox_fulldatabase.csv'
 
-
-class TQDMPredictCallback(Callback):
-    def __init__(self, custom_tqdm_instance=None, tqdm_cls=tqdm, **tqdm_params):
-        super().__init__()
-        self.tqdm_cls = tqdm_cls
-        self.tqdm_progress = None
-        self.prev_predict_batch = None
-        self.custom_tqdm_instance = custom_tqdm_instance
-        self.tqdm_params = tqdm_params
-
-    def on_predict_batch_begin(self, batch, logs=None):
-        pass
-
-    def on_predict_batch_end(self, batch, logs=None):
-        self.tqdm_progress.update(batch - self.prev_predict_batch)
-        self.prev_predict_batch = batch
-
-    def on_predict_begin(self, logs=None):
-        self.prev_predict_batch = 0
-        if self.custom_tqdm_instance:
-            self.tqdm_progress = self.custom_tqdm_instance
-            return
-
-        total = self.params.get('steps')
-        if total:
-            total -= 1
-
-        self.tqdm_progress = self.tqdm_cls(total=total, **self.tqdm_params)
-
-    def on_predict_end(self, logs=None):
-        if self.tqdm_progress and not self.custom_tqdm_instance:
-            self.tqdm_progress.close()
 
 def load_best_model(model_checkpoint, sweep_id):
     model, tokenizer = load_model_and_tokenizer(model_checkpoint)
@@ -73,20 +41,13 @@ def main():
     model, tokenizer = load_best_model(MODEL_CHECKPOINT, SWEEP_ID)
 
     df = pd.read_csv(DATA_FILE)
-    df = df.head(1_700)
+    df = df.head(1_000)
     dataset = load_test_data(df, tokenizer)
-
-    model.compile(
-        optimizer='Adam',
-        loss=tf.keras.losses.MeanSquaredError(),
-    )
 
     print("Computing predictions...")
     predictions = model.predict(
             dataset,
-            verbose=2,
-            batch_size=512,
-            #callbacks=[TQDMPredictCallback()],
+            batch_size=256,
             )
 
     df['predicted_cone_angle'] = predictions['logits']
